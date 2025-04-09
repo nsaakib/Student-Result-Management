07ba57445be147623e24d2468ae200f3ed229ef1
Mehedi Hasan Meraj <meraj.coding21@gmail.com>
2025-04-09 22:37:42 +0600

    Updated database

--- create_db.py
+++ create_db.py
@@ -3,8 +3,14 @@ def create_db():
     con = sqlite3.connect(database="rms.db")
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS course (cid INTEGER PRIMARY KEY AUTOINCREMENT, name text, duration text, charges text, description text)")
+    con.commit()
 
+    cur.execute("CREATE TABLE IF NOT EXISTS student (Roll INTEGER PRIMARY KEY ,Name text, Email text, Gender text, DOB text , Contact text, Admission text, Course text ,State text, City text, Pin text, Address text)")
+    con.commit()
 
+    cur.execute("CREATE TABLE IF NOT EXISTS result (rid INTEGER PRIMARY KEY AUTOINCREMENT, roll text, name text, course text, marks_ob text, full_marks text)")
+    con.commit()
 
+    con.close()
 
 create_db()
